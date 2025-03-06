033637274f9cfec71cfce095b0060a22770bd17d
Haris1906 <142414380+Haris1906@users.noreply.github.com>
2025-03-06 15:22:28 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@
 from __future__ import division
 from __future__ import print_function
 
-
 import sys
 import argparse
 import codecs
 import cv2
+import tensorflow as tf
 
 import editdistance
 from DataLoader import DataLoader, Batch
@@ -14,8 +14,10 @@ from Model import Model, DecoderType
 from SamplePreprocessor import preprocess
 
 import random
-import numpy as np 
+import numpy as np
 
+# Disable eager execution
+tf.compat.v1.disable_eager_execution()
 
 class FilePaths:
     "filenames and paths to data"
@@ -25,13 +27,12 @@ class FilePaths:
     fnInfer = '../data/3.jpg'
     fnCorpus = '../data/hindi_vocab.txt'
 
-
 def train(model, loader):
     "train NN"
-    epoch = 0 # number of training epochs since start
-    bestCharErrorRate = float('inf') # best valdiation character error rate
-    noImprovementSince = 0 # number of epochs no improvement of character error rate occured
-    earlyStopping = 5 # stop training after this number of epochs without improvement
+    epoch = model.lastEpoch  # Start from the last epoch
+    bestCharErrorRate = float('inf')
+    noImprovementSince = 0
+    earlyStopping = 5
     while True:
         epoch += 1
         print('Epoch:', epoch)
@@ -43,7 +44,7 @@ def train(model, loader):
             iterInfo = loader.getIteratorInfo()
             batch = loader.getNext()
             loss = model.trainBatch(batch)
-            print('Batch:', iterInfo[0],'/', iterInfo[1], 'Loss:', loss)
+            print('Batch:', iterInfo[0], '/', iterInfo[1], 'Loss:', loss)
 
         # validate
         charErrorRate = validate(model, loader)
@@ -53,8 +54,9 @@ def train(model, loader):
             print('Character error rate improved, save model')
             bestCharErrorRate = charErrorRate
             noImprovementSince = 0
-            model.save()
-            open(FilePaths.fnAccuracy, 'w').write('Validation character error rate of saved model: %f%%' % (charErrorRate*100.0))
+            model.save(epoch)  # Pass the current epoch to save
+            open(FilePaths.fnAccuracy, 'w').write(
+                'Validation character error rate of saved model: %f%%' % (charErrorRate * 100.0))
         else:
             print('Character error rate not improved')
             noImprovementSince += 1
@@ -63,7 +65,6 @@ def train(model, loader):
         if noImprovementSince >= earlyStopping:
             print('No more improvement since %d epochs. Training stopped.' % earlyStopping)
             break
-
 
 def validate(model, loader):
     "validate NN"
@@ -75,7 +76,7 @@ def validate(model, loader):
     numWordTotal = 0
     while loader.hasNext():
         iterInfo = loader.getIteratorInfo()
-        print('Batch:', iterInfo[0],'/', iterInfo[1])
+        print('Batch:', iterInfo[0], '/', iterInfo[1])
         batch = loader.getNext()
         (recognized, _) = model.inferBatch(batch)
 
@@ -86,23 +87,27 @@ def validate(model, loader):
             dist = editdistance.eval(recognized[i], batch.gtTexts[i])
             numCharErr += dist
             numCharTotal += len(batch.gtTexts[i])
-            print('[OK]' if dist==0 else '[ERR:%d]' % dist,'"' + batch.gtTexts[i] + '"', '->', '"' + recognized[i] + '"')
+            print('[OK]' if dist == 0 else '[ERR:%d]' % dist, '"' + batch.gtTexts[i] + '"', '->',
+                  '"' + recognized[i] + '"')
 
     # print validation result
     charErrorRate = numCharErr / numCharTotal
     wordAccuracy = numWordOK / numWordTotal
-    print('Character error rate: %f%%. Word accuracy: %f%%.' % (charErrorRate*100.0, wordAccuracy*100.0))
+    print('Character error rate: %f%%. Word accuracy: %f%%.' % (charErrorRate * 100.0, wordAccuracy * 100.0))
     return charErrorRate
-
 
 def infer(model, fnImg):
     "recognize text in image provided by file path"
-    img = preprocess(cv2.imread(fnImg, cv2.IMREAD_GRAYSCALE), Model.imgSize)
-    batch = Batch(None, [img])
-    (recognized, probability) = model.inferBatch(batch, True)
-    print('Recognized:', '"' + recognized[0].replace(" ", "") + '"')
-    print('Probability:', probability[0])
-
+    try:
+        img = preprocess(cv2.imread(fnImg, cv2.IMREAD_GRAYSCALE), Model.imgSize)
+        if img is None:
+            raise Exception("Image could not be read")
+        batch = Batch(None, [img])
+        (recognized, probability) = model.inferBatch(batch, True)
+        print('Recognized:', '"' + recognized[0].replace(" ", "") + '"')
+        print('Probability:', probability[0])
+    except Exception as e:
+        print(f"Error during inference: {e}")
 
 def infer_by_web(path, option):
     decoderType = DecoderType.BestPath
@@ -120,7 +125,6 @@ def infer_by_web(path, option):
     print('Probability:', probability[0])
     return recognized[0].replace(" ", ""), probability[0]
 
-
 def main():
     "main function"
     # optional command line args
@@ -128,7 +132,8 @@ def main():
     parser.add_argument("--train", help="train the NN", action="store_true")
     parser.add_argument("--validate", help="validate the NN", action="store_true")
     parser.add_argument("--beamsearch", help="use beam search instead of best path decoding", action="store_true")
-    parser.add_argument("--wordbeamsearch", help="use word beam search instead of best path decoding", action="store_true")
+    parser.add_argument("--wordbeamsearch", help="use word beam search instead of best path decoding",
+                        action="store_true")
     args = parser.parse_args()
 
     decoderType = DecoderType.BestPath
@@ -150,10 +155,10 @@ def main():
 
         # execute training or validation
         if args.train:
-            model = Model(loader.charList, decoderType)
+            model = Model(codecs.open(FilePaths.fnCharList, encoding='utf-8').read(), decoderType, mustRestore=False, lastEpoch=0)
             train(model, loader)
         elif args.validate:
-            model = Model(loader.charList, decoderType, mustRestore=False)
+            model = Model(codecs.open(FilePaths.fnCharList, encoding='utf-8').read(), decoderType, mustRestore=True, lastEpoch=0)
             validate(model, loader)
 
     # infer text on test image
@@ -162,7 +167,5 @@ def main():
         model = Model(codecs.open(FilePaths.fnCharList, encoding='utf-8').read(), decoderType, mustRestore=False)
         infer(model, FilePaths.fnInfer)
 
-
 if __name__ == '__main__':
     main()
-
